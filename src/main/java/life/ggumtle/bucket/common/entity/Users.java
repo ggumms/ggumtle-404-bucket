19d2f75fa2289bf78bf474538b74266f68ebb7ef
404 <schy2504@naver.com>
2024-07-16 00:34:56 +0900

    Seting #3: Spring Security setting
    - config
    - entity
    - exception
    - jwt
    - repository
    - response

---
+++ src/main/java/life/ggumtle/bucket/common/entity/Users.java
@@ -0,0 +1,44 @@
+package life.ggumtle.bucket.common.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@Table("users")
+public class Users implements UserDetails {
+
+    @Id
+    private Long id;
+
+    @Column("provider_id")
+    private String providerId;
+
+    @Column("internal_id")
+    private String internalId;
+
+    @Column("is_deleted")
+    private boolean isDeleted = false;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+}
